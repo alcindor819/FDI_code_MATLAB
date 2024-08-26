af2d210f90de0ccdb949d02b89818b52835ee481
alcindor819 <wangyunze@webmail.hzau.edu.cn>
2024-08-26 11:04:35 +0800

    Delete FDI/diagnostic directory

--- FDI/diagnostic/parsave.m
+++
@@ -1,4 +0,0 @@
-function parsave(feature0,ln2,feature_name)
-    feature3 = feature0;
-    save([ln2,feature_name],'feature3');
-end
--- FDI/diagnostic/r_parsave.m
+++
@@ -1,5 +0,0 @@
-function r_parsave(savename,chrname,read,region_len,dark_flag)
-    
-    save([savename,chrname],'read','region_len','dark_flag','-v7.3')
-    
-end
